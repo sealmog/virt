caea91605e39c9539ac9c7b7f332ff9cd59ce849
sealmo <sealmo@gmail.com>
2025-09-02 22:29:14 +0500

    sysctl

--- provisioning-kubeadm.sh
+++ provisioning-kubeadm.sh
@@ -4,8 +4,14 @@ set -e
 
 init() {
 
+cat <<EOF | tee /etc/sysctl.d/11-k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+sysctl -p
+
 # This overwrites any existing configuration in /etc/yum.repos.d/kubernetes.repo
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
@@ -22,6 +28,8 @@ systemctl enable --now kubelet
 
 helm() {
     curl -sfL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash -x -
+
+    ln -s /usr/local/bin/helm /usr/local/sbin/helm
 }
 
 run() {
