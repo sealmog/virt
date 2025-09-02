6ddd2e383937861ff48db4240f7cdba26c5ef4ab
sealmo <sealmo@gmail.com>
2025-09-02 16:29:28 +0500

    bash eof

--- provisioning.sh
+++ provisioning.sh
@@ -15,34 +15,34 @@ run() {
 
 rke2_pre() {
         
-    mkdir /var/log/kube-audit
+mkdir /var/log/kube-audit
 
-    cat <<EOF >> /etc/profile.d/rke2.sh
-    #!/bin/bash
-    ################################################################################
-    #                                                                              #
-    #    Set envvars for rke2 tools (crictl, kubectl)                              #
-    #                                                                              #
-    ################################################################################
+cat <<EOF >> /etc/profile.d/rke2.sh
+#!/bin/bash
+################################################################################
+#                                                                              #
+#    Set envvars for rke2 tools (crictl, kubectl)                              #
+#                                                                              #
+################################################################################
 
-    # Add /var/lib/rancher/rke2/bin to the path for sh compatible users
+# Add /var/lib/rancher/rke2/bin to the path for sh compatible users
 
-    if [ -z "${PATH-}" ] ; then
-      export PATH=/var/lib/rancher/rke2/bin
-    elif ! echo "${PATH}" | grep -q /var/lib/rancher/rke2/bin ; then
-      export PATH="${PATH}:/var/lib/rancher/rke2/bin"
-    fi
+if [ -z "${PATH-}" ] ; then
+    export PATH=/var/lib/rancher/rke2/bin
+elif ! echo "${PATH}" | grep -q /var/lib/rancher/rke2/bin ; then
+    export PATH="${PATH}:/var/lib/rancher/rke2/bin"
+fi
 
-    # Add config for crictl
+# Add config for crictl
 
-    if ! echo "${CRI_CONFIG_FILE-}" | grep -q /var/lib/rancher/rke2/agent/etc/crictl.yaml ; then
-      export CRI_CONFIG_FILE=/var/lib/rancher/rke2/agent/etc/crictl.yaml
-    fi
+if ! echo "${CRI_CONFIG_FILE-}" | grep -q /var/lib/rancher/rke2/agent/etc/crictl.yaml ; then
+    export CRI_CONFIG_FILE=/var/lib/rancher/rke2/agent/etc/crictl.yaml
+fi
 
-    # Add config for kubectl
+# Add config for kubectl
 
-    if ! echo "${KUBECONFIG-}" | grep -q /etc/rancher/rke2/rke2.yaml ; then
-      export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
-    fi
-    EOF
+if ! echo "${KUBECONFIG-}" | grep -q /etc/rancher/rke2/rke2.yaml ; then
+    export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
+fi
+EOF
 }
